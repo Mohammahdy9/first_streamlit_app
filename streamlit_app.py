a93e60c3d4b795b53ff746d6f8102d156ed1116e
Mohamed Mahdy <72105916+Mohammahdy9@users.noreply.github.com>
2023-02-27 16:15:58 -0600

    Update streamlit_app.py
    Added header
    Added menu items
    Deleted hello world text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-print('hello world')
+streamlit.header('hello world')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
