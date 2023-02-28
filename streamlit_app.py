e4ef1cf2cbed11165fc1f3714e1166353cd86ab6
Mohamed Mahdy <72105916+Mohammahdy9@users.noreply.github.com>
2023-02-28 11:29:49 -0600

    Update streamlit_app.py
    Added list of selected fruit from multiselect
    Display selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,16 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-#Setting the index to the Fruit column
+# Setting the index to the Fruit column
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Including a pick list to they can pick the fruit thet want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+# Including a pick list to they can pick the fruit thet want to include
+# Adding picked items to a list
+#
+fruit_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-#display the table on the page
-streamlit.dataframe(my_fruit_list)
+#display the selected fruits from table on the page
+streamlit.dataframe(fruits_to_show)
+
+streamlist.header(fruits_to_show)
