3ae3aeaa425fa0c1b0b3c0bc624d7f129f85e74e
Mohamed Mahdy <72105916+Mohammahdy9@users.noreply.github.com>
2023-03-02 10:22:32 -0600

    Update streamlit_app.py
    Added error exception handling block

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,19 +32,20 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 #Text entry box and sending input to fruityvice as part of api call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) just writes the raw data on the screen
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Normalizing the json version of our response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Output it on the screen as a table 
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-# Normalizing the json version of our response
-
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# Output it on the screen as a table 
-
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
