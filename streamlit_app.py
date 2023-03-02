e12e4c51bbfd1101793484268fe4dc84f1d4e5cd
Mohamed Mahdy <72105916+Mohammahdy9@users.noreply.github.com>
2023-03-02 10:44:42 -0600

    Update streamlit_app.py
    Move stop() statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,8 +73,8 @@ if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = inserted_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
-    
-streamlit.write('Thanks for adding ', add_my_fruit)
+
+streamlit.stop()
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
